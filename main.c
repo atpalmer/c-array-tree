7a6a92a94bb07618480631c9ff3afa2b227d576b
Andrew Palmer <atp@sdf.org>
2021-02-15 13:22:43 -0600

    Initialize repository

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int value;
+} Item;
+
+typedef struct {
+    int count;
+    Item *items[];
+} ArrayTree;
+
+ArrayTree *ArrayTree_new(void) {
+    static const int COUNT = 32;
+    ArrayTree *new = malloc(sizeof(ArrayTree) + COUNT * sizeof(Item *));
+    new->count = 0;
+    for(int i = 0; i < COUNT; ++i) {
+        Item *item = malloc(sizeof(Item));
+        item->value = rand() % 100;
+        new->items[i] = item;
+        ++new->count;
+    }
+    return new;
+}
+
+void ArrayTree_free(ArrayTree *this) {
+    for(int i = 0; i < this->count; ++i) {
+        free(this->items[i]);
+    }
+    free(this);
+}
+
+void ArrayTree_print(ArrayTree *this) {
+    for(int level = 0;;++level) {
+        int lstart = (1 << level) - 1;
+        int lcount = 1 << level;
+        for(int li = 0; li < lcount; ++li) {
+            int i = lstart + li;
+            if(i >= this->count) {
+                printf("\n");
+                return;
+            }
+            printf("%d ", this->items[i]->value);
+        }
+        printf("\n");
+    }
+}
+
+void ArrayTree_print_linear(ArrayTree *this) {
+    for(int i = 0; i < this->count; ++i) {
+        printf("%d ", this->items[i]->value);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    ArrayTree *tree = ArrayTree_new();
+    printf("TREE VIEW:\n");
+    ArrayTree_print(tree);
+    printf("ARRAY VIEW:\n");
+    ArrayTree_print_linear(tree);
+    ArrayTree_free(tree);
+}
